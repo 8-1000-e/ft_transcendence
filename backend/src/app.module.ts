e2a4ee7cf238e94d7418debef8461b0c0cde4dd0
NoctisLight <flush715@outlook.com>
2026-06-08 18:22:02 +0200

    chore: format code + ci lint check-only

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule {}
